6c6c734b3da10c1f9f5b871eeef62d71698458ec
Theo-Maes <theo27670mae@gmail.com>
2024-06-24 11:24:10 +0200

    join/leave group

---
+++ src/app/api/group/[id]/join/route.ts
@@ -0,0 +1,52 @@
+import { prisma } from "@/libs";
+import { UserGroup } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+interface UserGroupFormData {
+  userId: string;
+  groupId: string;
+}
+
+export async function POST(req: Request, params: { params: { id: string } }) {
+  const groupId = Number.parseInt(params.params.id);
+  const data = await req.formData();
+
+  const { userId } = Object.fromEntries(
+    data.entries()
+  ) as unknown as UserGroupFormData;
+
+  const userGroup = await prisma.userGroup.findFirst({
+    where: {
+      userId: Number.parseInt(userId),
+      groupId: groupId,
+    },
+  });
+
+  if (userGroup) {
+    return NextResponse.json(
+      { erreur: "Vous êtes déjà membre de ce groupe" },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const newGroup: UserGroup = await prisma.userGroup.create({
+      data: {
+        user: {
+          connect: {
+            id: Number.parseInt(userId),
+          },
+        },
+        group: {
+          connect: {
+            id: groupId,
+          },
+        },
+      },
+    });
+    return NextResponse.json({ data: newGroup }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ erreur: error }, { status: 500 });
+  }
+}
