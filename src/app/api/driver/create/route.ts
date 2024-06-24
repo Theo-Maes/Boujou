83f3633c3766014707a7e1656d0f05937a22c9c2
Theo-Maes <theo27670mae@gmail.com>
2024-06-24 09:30:41 +0200

    CRUD Driver

---
+++ src/app/api/driver/create/route.ts
@@ -0,0 +1,63 @@
+import { prisma } from "@/libs";
+import { Driver } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+interface DriverFormData {
+  adress: string;
+  city: string;
+  description: string;
+  endingdate: string;
+  latitude: string;
+  longitude: string;
+  quantity: string;
+  startingdate: string;
+  zipcode: string;
+  groupId: string;
+  userId: string;
+}
+
+export async function POST(req: Request) {
+  const data = await req.formData();
+
+  const {
+    adress,
+    city,
+    zipcode,
+    latitude,
+    longitude,
+    endingdate,
+    startingdate,
+    quantity,
+    groupId,
+    userId,
+  } = Object.fromEntries(data.entries()) as unknown as DriverFormData;
+
+  try {
+    const newDriver: Driver = await prisma.driver.create({
+      data: {
+        adress,
+        city,
+        endingdate,
+        latitude,
+        longitude,
+        quantity: Number.parseInt(quantity),
+        startingdate,
+        zipcode,
+        group: {
+          connect: {
+            id: Number.parseInt(groupId),
+          },
+        },
+        user: {
+          connect: {
+            id: Number.parseInt(userId),
+          },
+        },
+      },
+    });
+    return NextResponse.json({ data: newDriver }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ erreur: error }, { status: 500 });
+  }
+}
