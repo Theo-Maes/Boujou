953535eedcf06290843fd0c5a2902f2707811901
Theo-Maes <theo27670mae@gmail.com>
2024-06-26 11:36:11 +0200

    chore: Add admin events page and update menu

---
+++ src/app/admin/events/page.tsx
@@ -0,0 +1,55 @@
+import { Roboto } from "next/font/google";
+import Menu from "../menu";
+import Table from "./tableEvent";
+
+// export const metadata: Metadata = {
+//   title: "Boujou", //TODO
+// };
+
+// type User = {
+//   id: number;
+//   fullname: string;
+//   email: string;
+//   password: string | null;
+//   avatar: string;
+//   firstName: string;
+//   lastName: string;
+//   adress: string | null;
+//   zipcode: string | null;
+//   city: string | null;
+//   latitude: string | null;
+//   longitude: string | null;
+//   roleId: number;
+//   createdAt: Date;
+//   role: {
+//     name: string;
+//   };
+// };
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/event", {
+    cache: "no-store",
+  });
+
+  const jsonResponse = await response.json();
+
+  const data: any[] = jsonResponse.data;
+
+  return (
+    <main className="flex flex-col my-5">
+      <section id="top" className="pl-0 md:px-9 flex flex-row">
+        <div className="flex flex-row gap-10 flex-auto self-center">
+          <Menu labelActive="Evenements" />
+          <div className="flex flex-col w-full h-full gap-10">
+            <Table events={data} />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
