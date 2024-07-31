e6936a879056de786e9ec932d093787ca4bc32f9
Theo-Maes <theo27670mae@gmail.com>
2024-07-31 14:48:18 +0200

    chore: Add Lighthouse audits to GitHub Actions workflow for CI/CD pipeline

---
+++ .github/workflows/lighthouse/lighthouse-desktop.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "lighthouse:default",
+  settings: {
+    screenEmulation: {
+      mobile: false,
+      width: 1920,
+      height: 1080,
+    },
+    emulatedFormFactor: "desktop",
+    formFactor: "desktop",
+    onlyCategories: ["accessibility", "best-practices", "seo"],
+  },
+};
