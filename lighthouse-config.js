4e3bc2755f334fe58a1cf5fb3a1b458b632a7e1a
Theo-Maes <theo27670mae@gmail.com>
2024-07-31 09:55:37 +0200

    chore: Update Lighthouse configuration to include screenEmulation settings for desktop form factor

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: "lighthouse:default",
   settings: {
+    screenEmulation: {
+      mobile: false,
+      width: 1350,
+      height: 940,
+    },
+    emulatedFormFactor: "desktop",
     formFactor: "desktop",
     onlyCategories: ["accessibility", "best-practices", "seo"],
   },
