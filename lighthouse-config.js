a06f42d540d3bfdcc8f3c53dbbb0b49edadd0c17
Theo-Maes <theo27670mae@gmail.com>
2024-07-31 09:43:10 +0200

    chore: Update Lighthouse configuration to use only performance category in GitHub Actions workflow

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -1,12 +1,6 @@
 module.exports = {
   extends: "lighthouse:default",
   settings: {
-    emulatedFormFactor: "desktop",
-    audits: [
-      {
-        path: "metrics/first-contentful-paint",
-        options: { scorePODR: 800, scoreMedian: 1600 },
-      },
-    ],
+    onlyCategories: ["performance"],
   },
 };
